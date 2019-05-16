2e0904b1bd254fb0879c91c44b30cfdbdd8a1029
Yemi <yemiadebayo60@gmail.com>
2019-05-16 16:07:00 +0100

    Create attack for Player 1

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -4,3 +4,11 @@ feature 'Testing hit-points' do
     expect(page).to have_content "David: 50HP"
   end
 end
+
+feature 'Attacking' do
+  scenario 'attack Player 2' do
+    sign_in_and_play
+    click_link "Attack"
+    expect(page). to have_content 'James attacked David'
+  end
+end
